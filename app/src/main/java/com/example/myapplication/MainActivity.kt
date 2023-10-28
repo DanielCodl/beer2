7c51186af6ee90baccd7fa9cbff0f25ee50adbe5
DCODL <dd@dd.com>
2023-10-28 14:37:48 +0200

    nejake chujoviny

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val neco = findViewById<TextView>(R.id.tvHlavniLabel)
+        val rozmrdat = findViewById<EditText>(R.id.edKamZadatText)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            neco.text=rozmrdat.text.toString()
+        }
+
+
     }
 }
